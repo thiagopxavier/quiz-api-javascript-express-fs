2cc7a17b7eec4bd71d34c21a5b24c73be1596f76
Thiago Xavier <thiagox58@gmail.com>
2024-09-11 14:15:03 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ const quizRouter = require('./routers/quiz.router');
 
 const app = express();
 
-app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Permite todos os domínios
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.get('/', (_req, res) => {
   res.status(200).json('Olá, esta é uma API de quiz para diversos jogos.')
